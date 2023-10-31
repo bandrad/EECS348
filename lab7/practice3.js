6bc4be057ff33b44b15c44dcb859064d4ca9b1c1
bandrad <143830114+bandrad@users.noreply.github.com>
2023-10-30 22:44:43 -0500

    Update practice3.js

--- lab7/practice3.js
+++ lab7/practice3.js
@@ -1,14 +1,18 @@
 function verify(){
  
-  var password = document.getElementById(“password”).value;
-  var vPassword = document.getElementById(“confirm”).value;
+  var password = document.getElementById("password").value;
+  var vPassword = document.getElementById("confirm").value;
   
   if(password.length < 8) 
   {
-		alert(“Error: Password must be at least 8 characters”); 
+		alert("Error: Password must be at least 8 characters"); 
   }
+else if (vPassword.length < 8)
+{
+alert("Error: Second Password must be at leat 8 characters")
+}
   
-  if(password.value != vPassword.value)
+  else if(password.value != vPassword.value)
   {
     alert("Error: Passwords do not match!");
   }
